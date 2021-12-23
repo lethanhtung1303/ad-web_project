496c00ba3a5a8f4fd8a81a04b2471e0aa077e1ad
Trang <trang@example.com>
2021-12-23 21:54:43 +0700

    thêm phòng ban và list phòng ban

--- routes/addDept.js
+++ routes/addDept.js
@@ -2,10 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    var profile = req.session.profile;
-    var content = '../pages/addDept';
-    res.render('layouts/main', { profile, content });
-});
-
 module.exports = router;
+router.get('/', function (req, res, next) {
+     if(!req.session.profile)
+        return res.redirect('/login')
+    
+    var profile = req.session.profile
+    
+    if(profile.position !== 0)
+        return res.redirect('/login')
+    
+       var content = '../pages/addDept';
+
+    users.find({"position" : 2}).exec()``
+    .then(data => {
+        return res.render("layouts/main", {profile, content, data})
+    })
+    .catch(e => console.log(e))
+    
+});
