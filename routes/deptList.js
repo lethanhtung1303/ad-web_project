eb952967b93dd1aa62574a55b9c1fc58e3ae9170
lethanhtung <letung844gmail.com>
2021-12-24 15:45:56 +0700

    edit deptList

--- routes/deptList.js
+++ routes/deptList.js
@@ -5,22 +5,23 @@ const users = require('../models/users')
 router.get('/', function (req, res, next) {
      if(!req.session.profile)
         return res.redirect('/login')
-    
+
     var profile = req.session.profile
-    
+
     if(profile.position !== 0)
         return res.redirect('/login')
-    
+
        var content = '../pages/deptList';
 
     users.find({"position" : 2}).exec()
         .then(data => {
-            if(req.query.mess)
-                mess = req.query.mess
+            // if(req.query.mess)
+            //     mess = req.query.mess
+            var mess = req.query.mess || '';
             return res.render("layouts/main", {profile, mess, content, data})
         })
     .catch(e => console.log(e))
-    
+
 });
 module.exports = router;
 
